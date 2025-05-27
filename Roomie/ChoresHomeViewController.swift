168575027887bceb662d202515667a1e8f9dff79
Kira Brodsky <kbrods@uw.edu>
2025-05-26 23:36:16 -0700

    added empty screens

---
+++ Roomie/ChoresHomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChoresHomeViewController.swift
+//  Roomie
+//
+//  Created by Kira Brodsky on 5/26/25.
+//
+
+import UIKit
+
+class ChoresHomeViewController: UIViewController {
+
+    @IBOutlet weak var H1: UILabel!
+    @IBOutlet weak var Add: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        H1.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        
+        Add.layer.cornerRadius = Add.frame.width/2
+        Add.layer.masksToBounds = true
+        Add.titleLabel?.font = UIFont.systemFont(ofSize: 39, weight: .bold)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
