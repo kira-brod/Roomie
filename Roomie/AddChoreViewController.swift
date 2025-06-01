947c5710fb956a23f53564a5d70b5ce218422e67
MorVered7 <mvered9@gmail.com>
2025-06-01 15:17:40 -0700

    everything works except date is misaligned

---
+++ Roomie/AddChoreViewController.swift
@@ -0,0 +1,96 @@
+//
+//  AddChoreViewController.swift
+//  Roomie
+//
+//  Created by Mor Vered on 6/1/25.
+//
+
+import UIKit
+
+struct Chore {
+    let title: String
+    let date: Date
+    let assignedTo: String
+    let color: UIColor
+    let notes: String?
+}
+
+protocol AddChoreDelegate: AnyObject {
+    func didAddChore(_ chore: Chore)
+}
+
+class AddChoreViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
+    
+    let roommates = ["Roomie 1", "Roomie 2", "Roomie 3", "Roomie 4", "Roomie 5"]
+
+    let roommateColors: [String: UIColor] = [
+        "Roomie 1": .red,
+        "Roomie 2": .blue,
+        "Roomie 3": .green,
+        "Roomie 4": .yellow,
+        "Roomie 5": .purple
+    ]
+
+
+    @IBOutlet weak var choreName: UITextField!
+    @IBOutlet weak var choreDate: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Roomie.delegate = self
+        Roomie.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return roommates.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return roommates[row]
+    }
+
+    @IBOutlet weak var Roomie: UIPickerView!
+    
+   
+    @IBOutlet weak var choreNotes: UITextField!
+    
+    weak var delegate: AddChoreDelegate?
+
+    
+    @IBAction func add(_ sender: Any) {
+        print("Add button tapped")
+
+        let title = choreName.text ?? ""
+        
+        let date = choreDate.date
+            
+        let selectedRow = Roomie.selectedRow(inComponent: 0)
+        let assignedTo = roommates[selectedRow]
+        let color = roommateColors[assignedTo] ?? UIColor.gray
+            
+        let notes = choreNotes.text
+
+        let chore = Chore(title: title, date: date, assignedTo: assignedTo, color: color, notes: notes)
+            
+        delegate?.didAddChore(chore)
+            
+        dismiss(animated: true)
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Roomie/ChoresHomeViewController.swift
+++ Roomie/ChoresHomeViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
-class ChoresHomeViewController: UIViewController {
+class ChoresHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddChoreDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var H1: UILabel!
     @IBOutlet weak var Add: UIButton!
+    
+    var choresByDate: [Date: [Chore]] = [:]
+    var sortedDates: [Date] = []
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,8 +27,65 @@ class ChoresHomeViewController: UIViewController {
         Add.layer.cornerRadius = Add.frame.width/2
         Add.layer.masksToBounds = true
         Add.titleLabel?.font = UIFont.systemFont(ofSize: 39, weight: .bold)
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        //update empty state
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sortedDates.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let date = sortedDates[section]
+        return choresByDate[date]?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let date = sortedDates[section]
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter.string(from: date)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let date = sortedDates[indexPath.section]
+        let chore = choresByDate[date]![indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChoreCell", for: indexPath)
+        cell.textLabel?.text = chore.title
+        cell.textLabel?.textColor = chore.color
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        cell.detailTextLabel?.text = chore.notes
+        cell.detailTextLabel?.textColor = .gray
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 14)
+        return cell
+    }
+
     
+    func didAddChore(_ chore: Chore) {
+        let date = Calendar.current.startOfDay(for: chore.date)
+
+        if choresByDate[date] != nil {
+            choresByDate[date]?.append(chore)
+        } else {
+            choresByDate[date] = [chore]
+        }
+
+        sortedDates = choresByDate.keys.sorted()
+        tableView.reloadData()
+       //empty state update
+    }
+
+    
+    //empty state func
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAddChore", let destination = segue.destination as? AddChoreViewController {
+            destination.delegate = self
+        }
+    }
 
     /*
     // MARK: - Navigation
