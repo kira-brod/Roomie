16dac3bc12029b43cbed42cc4e12949968cc5a0e
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-06-03 19:30:03 -0700

    added table view logic for scheduling texts

--- Roomie/ScheduledTextsHomeViewController.swift
+++ Roomie/ScheduledTextsHomeViewController.swift
@@ -7,12 +7,43 @@
 
 import UIKit
 
-class ScheduledTextsHomeViewController: UIViewController {
+class TextCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+}
+
+class DateCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
+
+struct Text {
+    let title: String
+    let note : String
+}
+
+class ScheduledTextsHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in: UITableView) -> Int {
+        return texts.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts[dateKeys[section]]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
     @IBOutlet weak var H1: UILabel!
-    
     @IBOutlet weak var Add: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
+    // key -> date,
+    let texts : [String : [Text]] = [:]
+    let dateKeys : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +59,6 @@ class ScheduledTextsHomeViewController: UIViewController {
     
     @IBAction func AddAction(_ sender: Any) {
         performSegue(withIdentifier: "toScheduleDetails", sender: self)
-//        let vc = storyboard?.instantiateViewController(withIdentifier: "ScheduledTextsAddText") as! ScheduledTextsAddTextViewController
-//        
-//        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -38,7 +66,8 @@ class ScheduledTextsHomeViewController: UIViewController {
            let modalVC = segue.destination as? ScheduledTextsAddTextViewController {
             modalVC.onAddText = {
                 [weak self] text, date, note in
-                return
+                let newText = Text(title: text, note: note)
+                self?.tableView.reloadData()
             }
         }
     }
