116d1315285a30d580ae454fcf9b2a2d98b93a7f
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-06-05 02:07:04 -0700

    implemented schedule text functionality

--- Roomie/ScheduledTextsHomeViewController.swift
+++ Roomie/ScheduledTextsHomeViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 
-class TextCell {
+class TextCell: UITableViewCell {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var noteLabel: UILabel!
 }
 
-class DateCell {
-    @IBOutlet weak var dateLabel: UILabel!
-}
+//class DateCell: UITableViewCell {
+//    @IBOutlet weak var dateLabel: UILabel!
+//}
 
 struct Text {
     let title: String
+    let time: Date
     let note : String
 }
 
@@ -28,12 +29,65 @@ class ScheduledTextsHomeViewController: UIViewController, UITableViewDataSource,
         return texts.count
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return texts[dateKeys[section]]?.count ?? 0
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+//     configuring the section date headers
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = .systemBackground
+        
+        let topLine = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 1))
+                topLine.backgroundColor = .lightGray
+                header.addSubview(topLine)
+        
+        let date = dateKeys[section]
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+           label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+           label.textColor = .black
+        label.text = formatter.string(from: date)
+          header.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+             label.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 16),
+             label.trailingAnchor.constraint(equalTo: header.trailingAnchor, constant: -16),
+             label.topAnchor.constraint(equalTo: header.topAnchor, constant: 5),
+             label.bottomAnchor.constraint(equalTo: header.bottomAnchor, constant: -5)
+         ])
+        
+        return header
+    }
+    
+    // configuring prototype cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath) as! TextCell
+        // indexPath = [section: 0, row: 0]
+        let textArr = texts[dateKeys[indexPath.section]]
+        let textObj = textArr?[indexPath.row]
+        cell.titleLabel.text = textObj?.title
+        cell.noteLabel.text = textObj?.note
+        
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .none
+        let timeStr = formatter.string(from: textObj?.time ?? Date())
+        cell.timeLabel.text = timeStr
+        
+        return cell
     }
     
 
@@ -42,8 +96,8 @@ class ScheduledTextsHomeViewController: UIViewController, UITableViewDataSource,
     @IBOutlet weak var tableView: UITableView!
     
     // key -> date,
-    let texts : [String : [Text]] = [:]
-    let dateKeys : [String] = []
+    var texts : [Date : [Text]] = [:]
+    var dateKeys : [Date] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,7 +107,9 @@ class ScheduledTextsHomeViewController: UIViewController, UITableViewDataSource,
         Add.layer.cornerRadius = Add.frame.width/2
         Add.layer.masksToBounds = true
         Add.titleLabel?.font = UIFont.systemFont(ofSize: 39, weight: .bold)
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
         // Do any additional setup after loading the view.
     }
     
@@ -66,7 +122,26 @@ class ScheduledTextsHomeViewController: UIViewController, UITableViewDataSource,
            let modalVC = segue.destination as? ScheduledTextsAddTextViewController {
             modalVC.onAddText = {
                 [weak self] text, date, note in
-                let newText = Text(title: text, note: note)
+                // extracting the date
+                let calendar = Calendar.current
+                let componentsDate = calendar.dateComponents([.year, .month, .day], from: date)
+                let dateKey = calendar.date(from: componentsDate) ?? Date()
+                
+                // extracting the time
+//                let componentsTime = calendar.dateComponents([.hour, .minute], from: date)
+//                let today = calendar.startOfDay(for: Date())
+//                let timeOnly = calendar.date(byAdding: componentsTime, to: today)
+                
+                let newText = Text(title: text, time: date, note: note)
+                NSLog(note)
+                
+                // adding and sort
+                var sortedTexts = self?.texts[dateKey] ?? []
+                sortedTexts.append(newText)
+                sortedTexts.sort{ $0.time < $1.time}
+                self?.texts[dateKey] = sortedTexts
+                
+                self?.dateKeys = (self?.texts.keys.sorted())!
                 self?.tableView.reloadData()
             }
         }
--- ScheduledTextsAddTextViewController.swift
+++ ScheduledTextsAddTextViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ScheduledTextsAddTextViewController: UIViewController, UITextViewDelegate {
 
-    @IBOutlet weak var Cancel: UIButton!
+//    @IBOutlet weak var Cancel: UIButton!
     @IBOutlet weak var Notes: UITextView!
     
     @IBOutlet weak var Add: UIButton!
@@ -25,26 +25,20 @@ class ScheduledTextsAddTextViewController: UIViewController, UITextViewDelegate 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         reminderTitle.placeholder = "Reminder Title"
-
         Notes!.layer.borderWidth = 1
         Notes!.layer.borderColor = UIColor.lightGray.cgColor
         Notes!.layer.cornerRadius = 5.0
         Notes.text = "Notes"
         Notes.textColor = .lightGray
-        
         Notes.delegate = self
     }
     
+    // formatting notes placeholder
     func textViewDidEndEditing(_ textView: UITextView) {
         if Notes.text.isEmpty {
             Notes.text = "Notes"
             Notes.textColor = .lightGray
-        }
-        
-        if Notes == textView {
-            note = textView.text ?? ""
         }
     }
     
@@ -54,23 +48,17 @@ class ScheduledTextsAddTextViewController: UIViewController, UITextViewDelegate 
             Notes.text = ""
         }
     }
-    
-    
-    @IBAction func titleSet(_ sender: UITextField) {
-        textTitle = sender.text ?? ""
-    }
-    
-    @IBAction func dateChanged(_ sender: UIDatePicker) {
-        date = sender.date
-    }
-    
+
+    // passing data back to home VC, dismissing modal
     var onAddText: ((String, Date, String)-> Void)?
     
-    
     @IBAction func addText(_ sender: UIButton) {
+        note = Notes.textColor == .lightGray ? "" : Notes.text ?? ""
+        textTitle = reminderTitle.text ?? ""
+        date = datePicker.date
+        
         onAddText?(textTitle!, date ?? Date(), note!)
         dismiss(animated: true)
-
     }
     
     @IBAction func cancel(_ sender: UIButton) {
